60bdf1400d76076f4d3e79000da287f9e0ba6e3a
IdoD <idodror10@gmail.com>
2018-01-10 22:19:09 +0200

    Before upload

--- Program.cs
+++ Program.cs
@@ -20,7 +20,6 @@ namespace FilesService
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls("http://localhost:5020")
                 .Build();
     }
 }
